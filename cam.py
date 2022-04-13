af6ba6fb78e184957482026b51f4366feed04abd
Almir Menezes da Cunha Júnior <almirmdacunha@gmail.com>
2022-04-12 21:30:26 -0300

    feat(render): calculate pix center

--- cam.py
+++ cam.py
@@ -20,10 +20,21 @@ class Cam:
         self.l = l
         self.up = up
     
-    def render(self) -> np.ndarray:
-        w = (self.e-self.l)/np.linalg.norm(self.e-self.l)
-        u = np.cross(self.up, w)/np.linalg.norm(np.cross(self.up, w))
+    def render(self) -> list:
+        img = [[[0, 0, 0]]*self.h_res]*self.v_res
+
+        w = self.e-self.l
+        w = w/np.linalg.norm(w)
+
+        u = np.cross(self.up, w)
+        u = u/np.linalg.norm(u)
+
         v = np.cross(w, u)
 
         for (i, j) in itertools.product(range(self.v_res), range(self.h_res)):
-            pass
+            x = self.s*(j-(self.h_res/2)+0.5)
+            y = self.s*(i-(self.v_res/2)+0.5)
+
+            q = self.e+(x*u)+(y*v)-(self.d*self.w)
+            q = q/np.linalg.norm(q)
+            
