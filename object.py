9de921c68b8397595cb9a1744f194ad553ec8ab0
Almir Menezes da Cunha Júnior <almirmdacunha@gmail.com>
2022-04-12 22:17:45 -0300

    feat(object): create Sphere and Plane child classes

--- object.py
+++ object.py
@@ -3,3 +3,20 @@ import numpy as np
 class Object:
     def __init__(self, color: np.ndarray) -> None:
         self.color = color
+    
+    def intersection(self, o: np.ndarray, d: np.ndarray) -> float:
+        raise NotImplementedError
+
+class Sphere(Object):
+    def __init__(self, color: np.ndarray, center: np.ndarray, radius: float) -> None:
+        super().__init__(color)
+
+        self.c = center
+        self.r = radius
+
+class Plane(Object):
+    def __init__(self, color: np.ndarray, point: np.ndarray, normal: np.ndarray) -> None:
+        super().__init__(color)
+
+        self.p = point
+        self.n = normal
